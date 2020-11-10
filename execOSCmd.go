3fc658ce6faaffc61235a98395bee5d7220b86d4
jetlwx <37856993@qq.com>
2020-11-10 11:54:47 +0800

    Add files via upload

--- execOSCmd.go
+++ execOSCmd.go
@@ -50,7 +50,7 @@ func ExecOSCmdNew(command string, killInMilliSeconds time.Duration) (okResult bo
 	if isKilled && errbuf.String() == "" {
 		return false, outbuf.String(), "执行超时"
 	}
-	return okResult, outbuf.String(), errbuf.String()
+	return okResult, outbuf.String(), strings.Replace(errbuf.String(), "\n", "", -1)
 }
 
 //执行系统命令，errstr为空时则返回正常结果，当timeout =true时，timeoutNum值生效,
@@ -73,6 +73,30 @@ func ExecOSCmd(cmdstr string, timeout bool, timeoutNum int) (res string, errstr 
 	}
 
 	return "", "Command Invalid"
+}
+
+//执行系统命令，errstr为空时则返回正常结果，当timeoutNum =0时，默认12000秒，即不超时
+func ExecOSCmd3(cmdstr string, timeoutNum int) (ok bool, res string, errstr string) {
+	str := strings.Split(cmdstr, " ")
+	cmd := cmdr.New(true, str[0], str[1:]...)
+	if timeoutNum == 0 {
+		cmd.Options.Timeout = 12000
+	} else {
+		cmd.Options.Timeout = timeoutNum
+	}
+
+	if cmd.IsValid() {
+		out, err := cmd.Run()
+		if err != nil {
+			return false, "", string(out)
+			//fmt.Println("eeee:", string(comm.CustomeError(err)))
+		}
+
+		//fmt.Println(string(out))
+		return true, string(out), ""
+	}
+
+	return false, "", "Command Invalid"
 }
 
 //执行系统命令，errstr为空时则返回正常结果，当timeoutNum =0时，默认12000秒，即不超时
